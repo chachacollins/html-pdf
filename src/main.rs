c153c20042b4a8078dd4ac149a23dd7765acfa1e
cursedcadaver <collinschacha@hotmail.com>
2024-07-20 20:08:22 +0300

    added ability to save the html files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env::args;
+use std::fs;
+use std::process::Command;
+use std::str;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    let output = Command::new("curl")
+        .arg(args[1].clone())
+        .output()
+        .map_err(|err| {
+            eprintln!("failed to execute process: {}", err);
+            std::process::exit(1);
+        });
+
+    let binding = output.unwrap();
+    let pdf = str::from_utf8(&binding.stdout).unwrap();
+    let file = args[2].clone();
+    let create_file = fs::write(file, pdf).map_err(|err| {
+        eprintln!("failed to execute process: {}", err);
+        std::process::exit(1);
+    });
 }
